9ef38ec6815e68e6349545b333737384743a913b
Guillaume Gros <guillaume.gros@meta4.fr>
2018-01-06 22:10:28 +0100

    Prediction using trained Neural Network: FeedForward Propagation Prediction

--- ex3/predict.m
+++ ex3/predict.m
@@ -21,7 +21,22 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+% hidden layer has 25 nodes
+% size(Theta1) = 25   401 
+% size(X) =  [5000 400]
 
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
+
+% layer1 25 5000
+layer1 = sigmoid(X * Theta1');
+
+% Add ones in first column to the layer1 data matrix
+layer1 = [ones(size(layer1, 1), 1) layer1];
+% size layer1 = 5000 26
+
+% size(Theta2) = 10   26
+[max, p] = max(sigmoid(layer1 * Theta2'), [], 2);
 
 
 
