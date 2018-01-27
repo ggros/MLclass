802ffb99a703ea5d055dac84c86aab04586f072c
Guillaume Gros <guillaume.gros@meta4.fr>
2018-01-27 18:59:33 +0100

    ex6: find best C and sigma for dataset

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -8,8 +8,9 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+C = 0.3;
+sigma = 0.1;
+
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,10 +24,29 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+% loop thru all possible values 8x8
+% code disabled, we return directly the good values above
+% New Best found 0.035000 sigma = 0.100000 and C = 0.300000
 
-
-
-
+%{
+sigma_vec = [0.01 0.03 0.1 0.3 1 3 10 30]';
+C_vec = [0.01 0.03 0.1 0.3 1 3 10 30]';
+best_prediction = 1000;
+for i = 1:length(sigma_vec)
+  for j = 1:length(C_vec)
+    fprintf(['Train with sigma = %f and C = %f\n'], sigma_vec(i), C_vec(j));    
+    model= svmTrain(X, y, C_vec(j), @(x1, x2) gaussianKernel(x1, x2, sigma_vec(i)));
+    predictions = svmPredict(model, Xval);
+    prediction_error = mean(double(predictions ~= yval));
+    if(prediction_error < best_prediction)
+       fprintf(['New Best found %f sigma = %f and C = %f\n'], prediction_error, sigma_vec(i), C_vec(j)); 
+       best_prediction = prediction_error;
+       sigma = sigma_vec(i);
+       C = C_vec(j);   
+    end
+   end
+end
+%}
 
 
 % =========================================================================
