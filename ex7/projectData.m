53ffe892770a310842e10247f146ddb3c2624a5f
Guillaume Gros <guillaume.gros@meta4.fr>
2018-02-04 23:04:22 +0100

    ex7: pca, project Data

--- ex7/projectData.m
+++ ex7/projectData.m
@@ -17,8 +17,9 @@ Z = zeros(size(X, 1), K);
 %                    x = X(i, :)';
 %                    projection_k = x' * U(:, k);
 %
-
-
+K
+Ureduce = U(:, 1:K); % take first K
+Z = X * Ureduce; % projection on it
 
 
 % =============================================================
