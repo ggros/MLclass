4212db74dc3edf3a35688d4964c840678e5468f0
Guillaume Gros <guillaume.gros@meta4.fr>
2018-02-04 22:22:38 +0100

    ex7: find Closest Centroid - step 1 of K-Means

--- ex7/findClosestCentroids.m
+++ ex7/findClosestCentroids.m
@@ -20,8 +20,16 @@ idx = zeros(size(X,1), 1);
 %
 % Note: You can use a for-loop over the examples to compute this.
 %
-
-
+for(i=1:size(X,1))
+min = 0;
+for(j=1:K)
+  N = norm(X(i,:)-centroids(j,:))^2;
+  if(N<min || min == 0)
+    min = N;
+    idx(i) = j;
+  end
+end
+end
 
 
 
