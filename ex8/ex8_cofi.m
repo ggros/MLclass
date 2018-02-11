96df12e31ef79849240d71673697418dafdfb941
Guillaume Gros <guillaume.gros@meta4.fr>
2018-02-11 22:44:40 +0100

    ex8_cofi: changed some movies ratings to test recommendations

--- ex8/ex8_cofi.m
+++ ex8/ex8_cofi.m
@@ -131,10 +131,23 @@ my_ratings = zeros(1682, 1);
 my_ratings(1) = 4;
 
 % Or suppose did not enjoy Silence of the Lambs (1991), you can set
-my_ratings(98) = 2;
+my_ratings(98) = 4;
 
 % We have selected a few movies we liked / did not like and the ratings we
 % gave are as follows:
+
+my_ratings(2) = 4; % golden eyes
+my_ratings(11)= 5; % seven
+my_ratings(12)= 5; % usual suspects
+my_ratings(22) = 5;
+my_ratings(27)= 4;
+my_ratings(33)= 3;
+my_ratings(50) = 5;
+my_ratings(55) = 5;
+my_ratings(56) = 5;
+my_ratings(69)= 5;
+
+%{
 my_ratings(7) = 3;
 my_ratings(12)= 5;
 my_ratings(54) = 4;
@@ -144,6 +157,7 @@ my_ratings(69) = 5;
 my_ratings(183) = 4;
 my_ratings(226) = 5;
 my_ratings(355)= 5;
+%}
 
 fprintf('\n\nNew user ratings:\n');
 for i = 1:length(my_ratings)
